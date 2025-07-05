87ba21e8d42a0cd0b6c49d84dc00ecdd609c660f
Muhammad Ramzan <ramzan@Muhammads-MacBook-Pro.local>
2025-07-05 09:46:47 +0500

    project clean-up and lazy router is applied for home and projects page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
+<script setup lang="ts"></script>
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h2>Welcome to HomePage</h2>
+  <RouterLink to="/projects">Go to Projects</RouterLink>
 </template>
---
+++ src/views/ProjectsView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+<template>
+  <div>
+    <h2>Welcome to Projects Pages</h2>
+    <RouterLink to="/">Go to Home</RouterLink>
+  </div>
+</template>
