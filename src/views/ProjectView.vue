fd722650353a2e910381197758ee6af139bfbaae
Muhammad Ramzan <ramzan@Muhammads-MacBook-Pro.local>
2025-07-05 16:01:22 +0500

    routing done - not found page handled

---
+++ src/views/ProjectView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+const route = useRoute()
+const id = route.params.id
+</script>
+<template>
+  <div>
+    <h2>Welcome to Project Page{{ id }}</h2>
+    <RouterLink to="/">Go to Home</RouterLink>
+    <RouterLink to="/projects">Go to Projects</RouterLink>
+  </div>
+</template>
