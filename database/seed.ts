135812c0ae9f020f571db1f5fc14e84d1b885323
Muhammad Ramzan <ramzan@Muhammads-MacBook-Pro.local>
2025-07-06 13:42:44 +0500

    adding migration and seeding supabase db

--- database/seed.ts
+++ database/seed.ts
@@ -1,1 +1,38 @@
+import 'dotenv/config'
+import { createClient } from '@supabase/supabase-js'
 import { fakerEN_US as faker } from '@faker-js/faker'
+
+type Project = {
+  name: string
+  slug: string
+  status: 'in-progress' | 'completed'
+  collaborators: number[]
+}
+
+const supabase = createClient(
+  process.env.VITE_SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_SECRET!,
+)
+
+const createProjects = async (n: number) => {
+  const projects: Project[] = []
+
+  for (let i = 0; i < n; i++) {
+    const name = faker.lorem.words(3)
+    projects.push({
+      name: name,
+      slug: name.toLowerCase().replace(/ /g, '-'),
+      status: faker.helpers.arrayElement(['in-progress', 'completed']),
+      collaborators: faker.helpers.arrayElements([1, 2, 3]),
+    })
+  }
+
+  const { error } = await supabase.from('projects').insert(projects)
+  if (error) {
+    console.error('Insert error:', error)
+  } else {
+    console.log(`${n} projects inserted successfully`)
+  }
+}
+
+createProjects(10)
--- supabase/migrations/20250706034639_projects_schema.sql
+++ supabase/migrations/20250706034639_projects_schema.sql
@@ -4,7 +4,7 @@ create type project_status as enum('in-progress','completed');
 create table projects (
   id uuid primary key default gen_random_uuid(),
   name text not null,
-  description text,
+  slug text,
   status project_status not null default 'in-progress',
   created_at timestamp with time zone default now(),
   updated_at timestamp with time zone default now()
